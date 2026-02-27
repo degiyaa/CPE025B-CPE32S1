f3cb436c8a96750dcb57512c2633dc9ac2ef3d8b
degiyaa <degiyaa@gmail.com>
2026-02-27 14:44:48 +0800

    seatwork6.1

---
+++ mod6/seatwork6.1/script2.js
@@ -0,0 +1,7 @@
+let width = Number(prompt("Enter width", 0));
+let height = Number(prompt("Enter height", 0));
+let length = Number(prompt("Enter length", 0));
+
+let volume = width * height * length;
+
+console.log("Volume is", volume);
